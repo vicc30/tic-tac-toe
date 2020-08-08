854af0e7e76f37c065481ac2d24cd8fe3bf13a92
Victor Cruz <vicc23@gmail.com>
2020-08-08 00:44:21 -0500

    Added setState on click event

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    render() {
-      return (
-        <button className="square">
-          {this.props.value}
-        </button>
-      );
-    }
+  constructor(props) {
+      super(props);
+      this.state = {
+          value:null,
+      };
   }
+  /* use setState on click */
+  render() {
+    return (
+      <button className="square" 
+        onClick={() => this.setState({value:'X'})}> 
+        {this.state.value}
+      </button>
+    );
+  }
+}
   
   class Board extends React.Component {
     renderSquare(i) {
